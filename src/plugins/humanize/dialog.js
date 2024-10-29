a292d2577d4fec6fdef4d38f8649c7775e82e9ea
Sébastien Règne <regseb@gmail.com>
2024-10-29 19:15:00 +0100

    fix: Standardize random options.

--- src/plugins/humanize/dialog.js
+++ src/plugins/humanize/dialog.js
@@ -4,6 +4,10 @@
  * @author Sébastien Règne
  */
 
+/**
+ * @import { Page } from "playwright"
+ */
+
 import Random from "../../utils/random.js";
 import wait from "../../utils/wait.js";
 
@@ -12,18 +16,30 @@ import wait from "../../utils/wait.js";
  * possible. Par défaut le temps d'attente est pris aléatoirement entre `1` et
  * `5` secondes.
  *
- * @param {Object} [options]     Les options du plugin.
- * @param {number} [options.min] Le temps d'attente minimal en millisecondes.
- * @param {number} [options.max] Le temps d'attente maximal en millisecondes.
+ * @param {Object} [options]             Les options du plugin.
+ * @param {Object} [options.timeout]     Le temps d'attente.
+ * @param {number} [options.timeout.min] Le temps d'attente minimal en
+ *                                       millisecondes.
+ * @param {number} [options.timeout.max] Le temps d'attente maximal en
+ *                                       millisecondes.
+ * @returns {Record<string, Function>} Le crochet du plugin.
  */
 export default function dialogPlugin(options) {
-    const min = options?.min ?? 1000;
-    const max = options?.max ?? 5000;
+    const timeout = {
+        min: options?.timeout?.min ?? 1000,
+        max: options?.timeout?.max ?? 5000,
+    };
 
     return {
+        /**
+         * Ajoute un écouteur à l'ouverture d'une boite de dialogue.
+         *
+         * @param {Page} page La page nouvellement créée.
+         * @returns {Page} La page avec l'écouteur.
+         */
         "Page:new": (page) => {
             page.on("dialog", async (dialog) => {
-                await wait(Random.getInt(min, max));
+                await wait(Random.getInt(timeout.min, timeout.max));
                 await dialog.accept();
             });
             return page;
--- src/plugins/polyfill/viewport.js
+++ src/plugins/polyfill/viewport.js
@@ -7,36 +7,67 @@
 import Random from "../../utils/random.js";
 
 /**
- * Modifie la taille du navigateur.
+ * Modifie la taille de la zone d'affichage du navigateur.
  *
- * @param {Record<string, any>|undefined} options Les options de création d'un
- *                                                `Browser`.
- * @param {number}                        width   La largeur du navigateur.
- * @param {number}                        height  La hauteur du navigateur.
- * @returns {Record<string, any>|undefined} Les nouvelles options.
+ * @param {Record<string, any>|undefined} options    Les options de création
+ *                                                   d'un contexte ou d'une
+ *                                                   page.
+ * @param {Object}                        width      La largeur de la zone
+ *                                                   d'affichage.
+ * @param {number}                        width.min  La largeur minimum de la
+ *                                                   zone d'affichage.
+ * @param {number}                        width.max  La largeur maximum de la
+ *                                                   zone d'affichage.
+ * @param {Object}                        height     La hauteur de la zone
+ *                                                   d'affichage.
+ * @param {number}                        height.min La hauteur minimum de la
+ *                                                   zone d'affichage.
+ * @param {number}                        height.max La hauteur maximum de la
+ *                                                   zone d'affichage.
+ * @returns {Record<string, any>} Les nouvelles options.
  */
 const setViewport = function (options, width, height) {
     return {
         ...options,
-        viewport: { width, height, ...options?.viewport },
+        viewport: {
+            width: Random.getInt(width.min, width.max),
+            height: Random.getInt(height.min, height.max),
+            ...options?.viewport,
+        },
     };
 };
 
 /**
- * Crée un plugin pour faire varier la taille du navigateur. Par défaut les
- * valeurs sont prises aléatoirement entre _1000x500_ et _1800x800_.
+ * Crée un plugin pour faire varier la taille de la zone d'affichage du
+ * navigateur. Par défaut les valeurs sont prises aléatoirement entre
+ * _1000x500_ et _1800x800_.
  *
- * @param {Object} [options]        Les options du plugin.
- * @param {number} [options.width]  La largeur du navigateur.
- * @param {number} [options.height] La hauteur du navigateur.
+ * @param {Object} [options]            Les options du plugin.
+ * @param {Object} [options.width]      La largeur de la zone d'affichage.
+ * @param {number} [options.width.min]  La largeur minimum de la zone
+ *                                      d'affichage.
+ * @param {number} [options.width.max]  La largeur maximum de la zone
+ *                                      d'affichage.
+ * @param {Object} [options.height]     La hauteur de la zone d'affichage.
+ * @param {number} [options.height.min] La hauteur minimum de la zone
+ *                                      d'affichage.
+ * @param {number} [options.height.max] La hauteur maximum de la zone
+ *                                      d'affichage.
+ * @returns {Record<string, Function>} Les crochets du plugin.
  */
 export default function viewportPlugin(options) {
-    const width = options?.width ?? Random.getInt(1000, 1800);
-    const height = options?.height ?? Random.getInt(500, 800);
+    const width = {
+        min: options?.width?.min ?? 1000,
+        max: options?.width?.max ?? 1800,
+    };
+    const height = {
+        min: options?.height?.min ?? 500,
+        max: options?.height?.max ?? 800,
+    };
 
     return {
         /**
-         * Modifie les options de lancement du navigateur.
+         * Modifie les options de lancement d'un contexte (et du navigateur).
          *
          * @param {any[]} args Les paramètres de la méthode.
          * @returns {any[]} Les nouveaux paramètres.
@@ -56,7 +87,7 @@ export default function viewportPlugin(options) {
         },
 
         /**
-         * Modifie les options de création d'une page.
+         * Modifie les options de création d'une page (et de son contexte).
          *
          * @param {any[]} args Les paramètres de la méthode.
          * @returns {any[]} Les nouveaux paramètres.
